b673c478d7d79350985ecd6a5ece54d92f71c54b
karmilan <delftkarmilan@gmail.com>
2025-07-12 14:13:04 +0530

    dashboard ini

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,10 @@
+import Sidebar from "@/components/ui/Sidebar";
+import Topbar from "@/components/ui/Topbar";
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,10 +18,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={inter.className}>
+        <div className="flex min-h-screen">
+          <Sidebar />
+          <div className="flex-1 flex flex-col">
+            <Topbar />
+            <main className="flex-1 p-6 overflow-auto">{children}</main>
+          </div>
+        </div>
       </body>
     </html>
   );
