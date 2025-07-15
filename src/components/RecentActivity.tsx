e5a0385571e247cbd838a745036947575438c79a
karmilan <delftkarmilan@gmail.com>
2025-07-15 20:25:18 +0530

    Implement RecentActivity component and add recent activities data

--- src/components/RecentActivity.tsx
+++ src/components/RecentActivity.tsx
@@ -1,5 +1,35 @@
+import { recentActivities } from "@/utils/data";
+
 const RecentActivity = () => {
-  return <div>RecentActivity</div>;
+  return (
+    <>
+      <h1 className="text-xl font-semibold mb-2">Recent Activity</h1>
+      {recentActivities.map((data) => (
+        <div className="grid grid-cols-5 gap-4 mt-4" key={data.id}>
+          <div className="col-span-3">
+            <p className="text-sm">{data.user}</p>
+            <p className="text-sm text-gray-500">{data.activity}</p>
+          </div>
+          <div className="col-span-2">
+            <div className="flex justify-end ">
+              <p
+                className={`text-xs ${
+                  (data.action === "Create" &&
+                    "bg-green-300 dark:bg-green-800") ||
+                  (data.action === "Update" &&
+                    "bg-amber-300 dark:bg-amber-800") ||
+                  "bg-red-300 dark:bg-red-800"
+                } rounded-2xl py-1.5 px-3`}
+              >
+                {data.action}
+              </p>
+            </div>
+            <p className="text-sm text-gray-500 text-right">{data.time}</p>
+          </div>
+        </div>
+      ))}
+    </>
+  );
 };
 
 export default RecentActivity;
---
+++ src/utils/data.ts
@@ -0,0 +1,37 @@
+export const recentActivities = [
+  {
+    id: 1,
+    user: "jane doe",
+    activity: "Created project",
+    action: "Create",
+    time: "2 minutes ago",
+  },
+  {
+    id: 2,
+    user: "john smith",
+    activity: "Updated task",
+    action: "Update",
+    time: "5 minutes ago",
+  },
+  {
+    id: 3,
+    user: "alice johnson",
+    activity: "Deleted old tasks",
+    action: "Delete",
+    time: "10 minutes ago",
+  },
+  {
+    id: 4,
+    user: "john smith",
+    activity: "Created task",
+    action: "Create",
+    time: "5 minutes ago",
+  },
+  {
+    id: 5,
+    user: "alice johnson",
+    activity: "Deleted old tasks",
+    action: "Delete",
+    time: "10 minutes ago",
+  },
+];
